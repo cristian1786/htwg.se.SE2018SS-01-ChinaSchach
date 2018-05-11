37c9eb081c34c1db15b46db20e0fe100769e7c85
Cristian Weber <cristian_ntz@Cristians-MacBook-Pro.local>
2018-05-11 10:14:53 +0200

    First commit

---
+++ src/main/scala/de/htwg/se/ChinaSchach/Hello.scala
@@ -0,0 +1,13 @@
+package de.htwg.se.yourgame
+
+import de.htwg.se.yourgame.model.Student
+
+object Hello {
+  def main(args: Array[String]): Unit = {
+    val student = Student("Your Name")
+    println("Hello, " + student.name)
+    var x = 2
+    var y = x + 5
+    println("Hello I love big butts: " + y)
+  }
+}
