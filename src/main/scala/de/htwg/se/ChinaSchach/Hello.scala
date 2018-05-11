8db4232f22fee438301eeb5768bb06b9ad246da0
Cristian Weber <cristian_ntz@Cristians-MacBook-Pro.local>
2018-05-11 13:09:22 +0200

    Push after gitignore

--- src/main/scala/de/htwg/se/ChinaSchach/Hello.scala
+++ src/main/scala/de/htwg/se/ChinaSchach/Hello.scala
@@ -4,10 +4,11 @@ import de.htwg.se.yourgame.model.Student
 
 object Hello {
   def main(args: Array[String]): Unit = {
-    val student = Student("Your Name")
+    val student = Student("Cris")
     println("Hello, " + student.name)
     var x = 2
     var y = x + 5
     println("Hello I love big butts: " + y)
+    println(student.f(7))
   }
 }
