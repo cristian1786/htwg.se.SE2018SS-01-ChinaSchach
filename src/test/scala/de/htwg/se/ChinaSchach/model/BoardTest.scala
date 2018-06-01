75241f3aae17924ea21b42c9fe25f1f6ca94a72c
Miulle <mika.strandb@gmail.com>
2018-06-01 11:28:32 +0200

    Add simple test

---
+++ src/test/scala/de/htwg/se/ChinaSchach/model/BoardTest.scala
@@ -0,0 +1,17 @@
+package de.htwg.se.ChinaSchach.model
+
+import org.scalatest.junit.JUnitSuite
+import scala.collection.mutable.ListBuffer
+import org.junit.Assert._
+import org.junit.Test
+import org.junit.Before
+
+@RunWith(classOf[JUnitRunner])
+class BoardTest extends JUnitSuite {
+  var board = new Board
+  board.go()
+
+  @Test def verifyField(): Unit = {
+    assertEquals(board(0,0), Lance)
+  }
+}
