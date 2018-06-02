51d3b0281f30b3f9f9c3aab777e596a70ebe408f
cristian1786 <cristian_ntz@yahoo.com>
2018-06-02 14:24:21 +0200

    added movesAllowed and movesPossible prototypes

--- src/main/scala/de/htwg/se/ChinaSchach/model/King.scala
+++ src/main/scala/de/htwg/se/ChinaSchach/model/King.scala
@@ -3,6 +3,16 @@ package de.htwg.se.ChinaSchach.model
 case class King(side: String, promotable: String) extends Piece {
   //TODO: implement King Shogi
 
-  def moves()
+  def movesAllowed(source: (Int, Int), destination: (Int, Int)): Boolean = {
+    val allowed = false
+
+    allowed
+  }
+
+  def movesPossible(source: (Int, Int), destination: (Int, Int)): Boolean = {
+    val possible = false
+
+    possible
+  }
 
 }
--- src/main/scala/de/htwg/se/ChinaSchach/model/Piece.scala
+++ src/main/scala/de/htwg/se/ChinaSchach/model/Piece.scala
@@ -14,7 +14,7 @@ trait Piece {
     promotable
   }
 
-  def checkValidPoss(poss: Tuple2[Int, Int]): Boolean = {
+  def checkValidPoss(poss: (Int, Int)): Boolean = {
     var isValid: Boolean = false
     if (poss._1 >= 0 && poss._1 <= 9 && poss._2 >= 0 && poss._2 <= 9) {
       isValid = true
