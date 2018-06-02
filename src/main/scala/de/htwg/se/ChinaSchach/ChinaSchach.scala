3898ab381a75a96e87c616ffbd408023f08d564f
Miulle <mika.strandb@gmail.com>
2018-06-02 18:54:20 +0200

    Small fixes in controller and ChinaSchach.scala

--- src/main/scala/de/htwg/se/ChinaSchach/ChinaSchach.scala
+++ src/main/scala/de/htwg/se/ChinaSchach/ChinaSchach.scala
@@ -1,14 +1,11 @@
 package de.htwg.se.ChinaSchach
 
-import de.htwg.se.ChinaSchach.aview.Tui
 import de.htwg.se.ChinaSchach.controller.Controller
-import de.htwg.se.ChinaSchach.model.Board
-//import scala.util.control.Breaks._
+
 
 object ChinaSchach {
   def main(args: Array[String]): Unit = {
-    val controller = new Controller("Peter", "Opa")
+    val controller = new Controller("Peter", "Hans")
     controller.boardInit()
-
   }
 }
--- src/main/scala/de/htwg/se/ChinaSchach/controller/controller.scala
+++ src/main/scala/de/htwg/se/ChinaSchach/controller/controller.scala
@@ -1,16 +1,20 @@
 package de.htwg.se.ChinaSchach.controller
 
 import de.htwg.se.ChinaSchach.aview.Tui
-import de.htwg.se.ChinaSchach.model.{Board, Piece, Player}
+import de.htwg.se.ChinaSchach.model.{Board, Player}
 
 //TODO: implement Controller
 class Controller(name1: String, name2: String) {
 
   var board = new Board
+
   val tui = new Tui(board)
 
   val player1 = new Player(name1)
   val player2 = new Player(name2)
+
+  var listPlayer1 = _
+  var listPlayer2 = _
 
   //initializes playing board
   def boardInit() : Unit = {
@@ -24,8 +28,8 @@ class Controller(name1: String, name2: String) {
   //initialize player
   def playerInit(name1: String, name2: String) : Unit = {
 
-    val listPlayer1 = player1.setFigures(board, "top")
-    val listPlayer2 = player2.setFigures(board, "bottom")
+    listPlayer1 = player1.setFigures(board, "top")
+    listPlayer2 = player2.setFigures(board, "bottom")
 
     tui.outputPlayerFigures(listPlayer1)
     tui.outputPlayerFigures(listPlayer2)
