a55836cf7861238708bb384e32396184f29ae877
Miulle <mika.strandb@gmail.com>
2018-06-02 18:57:29 +0200

    Fix format

--- src/main/scala/de/htwg/se/ChinaSchach/controller/controller.scala
+++ src/main/scala/de/htwg/se/ChinaSchach/controller/controller.scala
@@ -1,24 +1,23 @@
 package de.htwg.se.ChinaSchach.controller
 
 import de.htwg.se.ChinaSchach.aview.Tui
-import de.htwg.se.ChinaSchach.model.{Board, Player}
+import de.htwg.se.ChinaSchach.model.{Board, Piece, Player}
+
+import scala.collection.mutable.ListBuffer
+
 
 //TODO: implement Controller
 class Controller(name1: String, name2: String) {
 
   var board = new Board
-
   val tui = new Tui(board)
-
   val player1 = new Player(name1)
   val player2 = new Player(name2)
-
-  var listPlayer1 = _
-  var listPlayer2 = _
+  var listPlayer1: ListBuffer[Piece] = _
+  var listPlayer2: ListBuffer[Piece] = _
 
   //initializes playing board
   def boardInit() : Unit = {
-    //TODO: initialze Field
     board.go()
     tui.outputField()
     playerInit("Peter", "Hannes")
