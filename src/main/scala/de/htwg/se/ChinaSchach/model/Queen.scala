0f79ae21e8e445e8cd4816d21fd3a637dfc61404
cristian1786 <cristian_ntz@yahoo.com>
2018-06-16 15:18:14 +0200

    changes to Chess

--- src/main/scala/de/htwg/se/ChinaSchach/model/Queen.scala
+++ src/main/scala/de/htwg/se/ChinaSchach/model/Queen.scala
@@ -2,7 +2,7 @@ package de.htwg.se.ChinaSchach.model
 
 import scala.collection.mutable.ListBuffer
 
-case class Gold(side: String, promotable: String) extends Piece {
+case class Queen(side: String, promotable: String) extends Piece {
   //TODO: Implementation of Gold Shogi
   override val possibleMoves: ListBuffer[(Int, Int)] = ListBuffer((-1, 0), (0, -1), (1, 0), (1, 1), (0, 1), (-1, 1))
 }
